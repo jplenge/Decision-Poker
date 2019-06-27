7f1094829f9b38882384b768d8026a3617e5ac57
Jodi Szarko <jszarko@jodis-macbook.local>
2019-06-27 13:48:52 +0200

    Initial Commit

---
+++ DecisionPoker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DecisionPoker
+//
+//  Created by Jodi Szarko on 6/27/19.
+//  Copyright © 2019 Jodi Szarko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
