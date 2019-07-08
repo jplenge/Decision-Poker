0c2bfc46d550a0dd4ad5e9677ed19923f3c2f8d2
Jodi Szarko <jszarko@jodis-macbook.local>
2019-07-08 22:37:34 +0200

    ...

---
+++ DecisionPoker/Core Data/SavedCard+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  SavedCard+CoreDataProperties.swift
+//  DecisionPoker
+//
+//  Created by Jürgen Plenge on 08.07.19.
+//  Copyright © 2019 Jodi Szarko. All rights reserved.
+//
+//
+
+import Foundation
+import CoreData
+
+
+extension SavedCard {
+
+    @nonobjc public class func createFetchRequest() -> NSFetchRequest<SavedCard> {
+        return NSFetchRequest<SavedCard>(entityName: "SavedCard")
+    }
+
+    @NSManaged public var cardName: String?
+    @NSManaged public var cardComment: String?
+    @NSManaged public var savedParentDeck: SavedDeck?
+
+}
